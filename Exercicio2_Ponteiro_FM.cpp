bd25fd5b7160dbdfc0b1083eb93591e26358642a
Felipe Menegat <felipe_menegat@hotmail.com>
2017-08-03 10:52:46 -0300

    Add files via upload

---
+++ Exercicio2_Ponteiro_FM.cpp
@@ -0,0 +1,27 @@
+/*
+	Exercicio 2
+	Felipe Menegat
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main () {	
+	
+int y[7]={80, 70, 60, 50, 40, 30, 20}; // VETOR
+int *py; // PONTEIRO
+int n; // decrecmentadora
+n=0;
+	py=&y[n]; //
+	
+	for (n=7;n>0;n--){
+		py = &y[n];
+		printf("%d\n", *(py+n));	
+		printf("%d\n", (py+n));
+		printf("%d\n", &y[n]);
+
+	}
+system("pause");
+return 0;	
+
+}
