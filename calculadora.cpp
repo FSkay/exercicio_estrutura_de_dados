d4da10456c58c4c0f04f9c9eb07b534986bb6c15
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-06 01:09:12 -0300

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,9 +1,11 @@
-/*/////////////////////////////////////////////////////////////////////////////////////////////	
-//	Utilizando o conceito de protótipos, escreva um programa com 5 opções: 		     //	
-//	4 para realizar as operações matemáticas básicas, e 1 para encerrar o programa. Ex:  //
-//	1 – Adição 2 – Subtração 3 – Multiplicação 4 – Divisão 0 – Sair                      //
-//	Aluno F.Menegat                                                                      //
-*//////////////////////////////////////////////////////////////////////////////////////////////
+/*
+|---------------------------------------------------------------------------------------------|	
+|	Utilizando o conceito de protótipos, escreva um programa com 5 opções: 		      |
+|	4 para realizar as operações matemáticas básicas, e 1 para encerrar o programa. Ex:   |
+|	1 – Adição 2 – Subtração 3 – Multiplicação 4 – Divisão 0 – Sair                       |
+|	Aluno F.Menegat                                                                       |
+|---------------------------------------------------------------------------------------------|
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
