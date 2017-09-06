ef90428d0ecf199e0afba604eefd4fb4424af648
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-06 02:55:06 -0300

    Update fazenda.cpp

--- fazenda.cpp
+++ fazenda.cpp
@@ -4,28 +4,28 @@
 
 main() {
 	setlocale(LC_ALL,"Portuguese");
-	int nPorcos, abaixoDaMedia, i;
-	float *pesoPorcos;
-	float somaPorcos, mediaPorcos;
-	printf("Digite o número de porcos: ");
-	scanf("%i", &nPorcos);
-	pesoPorcos = (float*) malloc(nPorcos*sizeof(float));
+	int nTerneiro, abaixoDaMedia, i;
+	float *pesoTerneiro;
+	float somaTerneiro, mediaTerneiro;
+	printf("Digite o número de terneiros: ");
+	scanf("%i", &nTerneiro);
+	pesoTerneiro = (float*) malloc(nTerneiro*sizeof(float));
 	if (pesoPorcos) {
-		for (i = 0; i < nPorcos; i++) {
-			printf("- PESO DO PORCO %i: ", i + 1);
-			scanf("%f", &pesoPorcos[i]);
-			somaPorcos = somaPorcos + pesoPorcos[i];
-			mediaPorcos = somaPorcos / nPorcos;
+		for (i = 0; i < nTerneiro; i++) {
+			printf("- PESO DO TERNEIRO %i: ", i + 1);
+			scanf("%f", &pesoTerneiro[i]);
+			somaTerneiro = somaTerneiro + pesoTerneiro[i];
+			mediaTerneiro = somaTerneiro / nTerneiro;
 		}
-		for (i = 0; i < nPorcos; i++) {
-			printf("PORCO %i: %.2f\n", i + 1, *(pesoPorcos + i));
+		for (i = 0; i < nTermeorp; i++) {
+			printf("TERNEIRO %i: %.2f\n", i + 1, *(pesoTerneiro + i));
 		}
-		for (i = 0; i < nPorcos; i++) {
-			if (pesoPorcos[i] < mediaPorcos) {
+		for (i = 0; i < nTerneiro; i++) {
+			if (pesoTerneiro[i] < mediaTerneiro) {
 				abaixoDaMedia++;
 			}
 		}
-		printf("Média de Peso dos Porcos: %.2fKG\n", mediaPorcos);
-		printf("Porcos Abaixo da Média: %i", abaixoDaMedia);
+		printf("Média de peso dos terneiros: %.2fKG\n", mediaTerneiro);
+		printf("Terneiro abaixo da Média: %i", abaixoDaMedia);
 	}
 }
