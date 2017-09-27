9ba57d8a702efcd6e6fcd75a6be8d7075b302a48
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-27 20:04:55 -0300

    Create PVESEX9.c

---
+++ PVESEX9.c
@@ -0,0 +1,28 @@
+*/
+Fazer um “programinha” para cadastrar Funcionários de uma Firma. O programa deve:
+
+a)Conter um “menuzinho” conforme exemplo:
+ Cadastro de Funcionários
+1- Incluir novo Funcionário
+2-Exibir Funcionário cadastrados
+3-Pesquisar Funcionários
+4-Excluir Funcionários
+5- Sair do Programa
+
+b)Fazer inclusão dos funcionários(maximo 99 alunos) por meio de uma lista linear.
+c)Exibir a lista na tela quando solicitado
+d)Pesquisar funcionário
+e) %ferecer uma opção para sair do programa
+f) Dados a serem armazenados:
+
+Código Funcionário: tipo inteiro;
+Nome: vetor char máximo 30 caracteres;
+CPF: do tipo float.
+Telefone: tipo inteiro
+Endereço: tipo char 60 caracteres
+e-mail: tipo char 30 caracteres
+
+Aluno: Felipe Menegat
+/*
+
+#include <stdlib.h>
